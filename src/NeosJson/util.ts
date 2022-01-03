038abfd69ea5781688e1502e837fe6905d52530e
rassi0429 <taitokokoa+rassi@gmail.com>
2022-01-04 06:53:38 +0900

    feat: util

---
+++ src/NeosJson/util.ts
@@ -0,0 +1,11 @@
+import { Slot } from "./slot"
+import { v4 as uuidv4 } from "uuid"
+import { NeosJson } from "./index"
+
+export class NeosJsonUtil {
+  static exportJson (json: NeosJson): string {
+    return JSON.stringify({
+      Object: json.getRootSlot()
+    })
+  }
+}
