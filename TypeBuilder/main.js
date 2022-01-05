edf2e5a25f40e2dd2ea2f7e0830954194c2a2b66
rassi0429 <taitokokoa+rassi@gmail.com>
2022-01-05 22:59:13 +0900

    WIP: typebuilder

--- TypeBuilder/main.js
+++ TypeBuilder/main.js
@@ -4,25 +4,35 @@ const path = require("path/win32")
 
 components.forEach(c => {
   console.log(c.name)
-  if (c.name.includes("<")) return
+  // if (c.name.includes("<")) return
+  c.name = c.name.replace("<", "").replace(">", "").replace(",", "")
   const code = getCode(c)
-  const p = path.join(path.resolve("../"), "src/NeosJson", "Components", c.pathName)
+  const p = path.join(path.resolve("../"), "src/NeosJson", "Components", c.pathName.replace("<", "").replace(">", "").replace(",", ""))
   fs.mkdirSync(path.dirname(p), { recursive: true })
   fs.writeFileSync(p + ".ts", code)
   // console.log(code)
 })
 
 function getCode (arg) {
-  const list = arg.syncmembers.filter(i => !(i.name == "Enabled" || i.name == "UpdateOrder" || i.name == "persistent"))
-  const classMember = list.map(i => { return `${i.name} = new ${getType(i.type)}()` })
-  const inputMember = list.map(i => { return `${i.name}?: ${getInputType(i.type)}` })
+  const list = arg.syncmembers.filter(i => !(i.name === "Enabled" || i.name === "UpdateOrder" || i.name === "persistent"))
+  const classMember = list.map(i => {
+    return `${i.name} = new ${getType(i.type)}()`
+  })
+  const inputMember = list.map(i => {
+    return `${i.name}?: ${getInputType(i.type)}`
+  })
+
+  let generic = ""
+  if (arg.genericTypes) {
+    generic = arg.genericTypes.map(t => `"${t.type}"`).join(" | ")
+  }
 
   return `
 import { ComponentBase, ComponentDataBase, ComponentDataBool, ComponentDataNumber, ComponentDataString, ComponentDataUnknown} from "NeosJson/Components"
 export class ${arg.name} extends ComponentBase {
   Data: ${arg.name}Interface
-  constructor (init?: Input${arg.name}) {
-    super("${arg.fullName}")
+  constructor (${generic ? "type: " + (generic || "string") + "," : ""}init?: Input${arg.name}) {
+    ${generic ? "super(type)" : `super("${arg.fullName}")`}
     this.Data = new ${arg.name}Interface(init)
   }
 }
